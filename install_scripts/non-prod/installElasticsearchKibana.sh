1969dc349545f77b675dd3f1e1c3011fe9a7c43f
Johan Louwers <louwersj@gmail.com>
2016-11-16 15:09:18 +0100

    Update installElasticsearchKibana.sh

--- install_scripts/non-prod/installElasticsearchKibana.sh
+++ install_scripts/non-prod/installElasticsearchKibana.sh
@@ -3,10 +3,9 @@
 
 function runMain {
   installJava
+  configureYum
   installElasticsearch
-  startElasticsearch
   installKibana
-  startKibana
 }
 
 
@@ -34,14 +33,7 @@ function installJava {
   fi
 }
 
-
-
-function installElasticsearch {
-  elasticsearcInstalled=`packageInstalled elasticsearch`
-    if [ "$elasticsearcInstalled" = "true" ];
-   then
-       echo "elasticsearch is already installed"
-   else
+function configureYum {
        echo "importing elastic GPG key"
        rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
 
@@ -55,17 +47,22 @@ function installElasticsearch {
        echo "enabled=1" >> /etc/yum.repos.d/public-yum-ol6.repo
        echo "autorefresh=1" >> /etc/yum.repos.d/public-yum-ol6.repo
        echo "type=rpm-md" >> /etc/yum.repos.d/public-yum-ol6.repo
+}
 
+
+function installElasticsearch {
+  elasticsearcInstalled=`packageInstalled elasticsearch`
+    if [ "$elasticsearcInstalled" = "true" ];
+   then
+       echo "elasticsearch is already installed"
+   else
        echo "installing elasticsearch"
        yum -y install elasticsearch
+
+       startElasticsearch
    fi
 }
 
-
-function startElasticsearch {
-  echo "starting elasticsearch"
-  service elasticsearch start
-}
 
 function installKibana {
   kibanaInstalled=`packageInstalled kibana`
@@ -73,26 +70,27 @@ function installKibana {
    then
        echo "Kibana is already installed"
    else
-  #     echo "importing elastic GPG key"
-  #     rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
-
-#       echo "adding kibana repository to yum"
-#       echo "" >> /etc/yum.repos.d/public-yum-ol6.repo
-#       echo "[kibana-5.x]" >> /etc/yum.repos.d/public-yum-ol6.repo
-#       echo "name=Kibana repository for 5.x packages" >> /etc/yum.repos.d/public-yum-ol6.repo
-#       echo "baseurl=https://artifacts.elastic.co/packages/5.x/yum" >> /etc/yum.repos.d/public-yum-ol6.repo
-#       echo "gpgcheck=1" >> /etc/yum.repos.d/public-yum-ol6.repo
-#       echo "gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch" >> /etc/yum.repos.d/public-yum-ol6.repo
-#       echo "enabled=1" >> /etc/yum.repos.d/public-yum-ol6.repo
-#       echo "autorefresh=1" >> /etc/yum.repos.d/public-yum-ol6.repo
-#       echo "type=rpm-md" >> /etc/yum.repos.d/public-yum-ol6.repo
-       
        echo "installing kibana"
        yum -y install kibana
+
+       startKibana
    fi
 }
 
+
+function startElasticsearch {
+  echo "configure elasticsearch service"
+  chkconfig --add elasticsearch
+
+  echo "starting elasticsearch"
+  service elasticsearch start
+}
+
+
 function startKibana {
+  echo "configure kibana service"
+  chkconfig --add kibana
+
   echo "starting Kibana"
   service kibana start
 }

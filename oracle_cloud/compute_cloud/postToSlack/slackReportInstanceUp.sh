c22422099053290fcd2e5e104e77fe7011480f0c
Johan Louwers <louwersj@gmail.com>
2017-02-20 20:19:23 +0100

    Update slackReportInstanceUp.sh

--- oracle_cloud/compute_cloud/postToSlack/slackReportInstanceUp.sh
+++ oracle_cloud/compute_cloud/postToSlack/slackReportInstanceUp.sh
@@ -1,5 +1,41 @@
 #!/usr/bin/env bash
-
+#!/bin/bash
+# NAME:
+#  slackReportInstanceUp.sh 
+#
+# DESC:
+#  To be used to install a nginx server in a very quick and dirty manner.
+#  This is only for building a quick and dirty test/dev server and you should
+#  never .... never never never .... use this for a production setup. 
+#  You will need nginx-1.8.1-1.el7.ngx.x86_64.rpm which can be downloaded 
+#  from the NGINX website. You need to ensure that this file is in the same 
+#  directory as this script to make the script work. 
+#
+# This script is tested for Oracle Linux.
+#
+#
+# LOG:
+# VERSION---DATE--------NAME-------------COMMENT
+# 0.1       20FEB17     Johan Louwers    Initial creation
+#
+# LICENSE:
+# Copyright (C) 2017  Johan Louwers
+#
+# This code is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This code is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this code; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# *
+# */
 # Retrieve meta-data from the internal cloud API used to populate the slack message.
 # includes the instance type, local IP and the local FQDN a registered in the OPC.
  metaDataInstanceType="$(curl -m 5 --fail -s http://192.0.0.192/1.0/meta-data/instance-type)"

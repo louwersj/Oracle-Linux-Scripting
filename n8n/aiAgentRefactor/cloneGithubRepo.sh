6ab19b080f6089b2466552b1b9773982934af0ef
Johan Louwers <louwersj@gmail.com>
2025-01-31 14:11:05 +0100

    Create cloneGithubRepo.sh

---
+++ n8n/aiAgentRefactor/cloneGithubRepo.sh
@@ -0,0 +1,12 @@
+#!/bin/sh  
+
+# Assign command-line arguments to variables
+gitUsername=$1  # First argument: GitHub username
+gitPat=$2       # Second argument: GitHub personal access token (PAT)
+gitRepo=$3      # Third argument: Repository name
+
+# Clone the specified GitHub repository into a temporary directory
+git clone https://$gitUsername:$gitPat@github.com/$gitUsername/$gitRepo /tmp/P$gitRepo
+
+# Print the repository name to the console
+echo $gitRepo
